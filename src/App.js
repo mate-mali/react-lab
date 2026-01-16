1dcffec7bec8f59e61d794a44c374ebeae53339a
Mateusz <mateusz.malinowski.1991@gmail.com>
2026-01-16 23:32:10 +0100

    Cwiczenie Listy Elementow

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import {useState} from "react";
 function App() {
     const [title, setTitle] = useState('Fight Club');
     const [h3, setH3] = useState('Wpisz coś czarodzieju');
+    const [movies, setMovies] = useState([
+        {title: "Wall-E"},
+        {title: "Pulp Fiction"},
+        {title: "Matrix"},
+        {title: "1670"}
+    ]);
     function handleChange(event) {
         //console.log(event.target.value);
         setTitle(event.target.value);
@@ -15,17 +21,24 @@ function App() {
     }
     function handleClick() {
         alert(title);
+        setMoviesList();
     }
-
-
+    function setMoviesList() {
+      setMovies([...movies, {title: title}]);
+    }
+    
     return (
         <div>
             <h1>My favourite movies to watch</h1>
+            <h3>Titles</h3>
+            <ul>
+              {movies.map((movie)=> <li key={movie.title}>{movie.title}</li>)}
+            </ul>
             <h2>My favourite movie for today is {title}</h2>
-
-            <h3>{h3}</h3>
+            <h4>{h3}</h4>
             <input type="text" onChange={handleChange} value={title}/>
             <input type="button" onClick={handleClick} value={"Kliknij nie wstydz się"}/>
+            
         </div>
     );
 }
