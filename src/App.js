3e1fa29c3a46aa552f58ccbe2bfb644940258058
Mateusz <mateusz.malinowski.1991@gmail.com>
2026-01-16 23:16:17 +0100

    cwiczenie reaktywnosc

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {useState} from "react";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [title, setTitle] = useState('Fight Club');
+    const [h3, setH3] = useState('Wpisz coś czarodzieju');
+    function handleChange(event) {
+        //console.log(event.target.value);
+        setTitle(event.target.value);
+        if(event.target.value.length <= 3){
+            setH3("Tytul ma mniej niż 3 znaki");
+        } else {
+            setH3("Tytul jest daijobu");
+        }
+    }
+
+
+    return (
+        <div>
+            <h1>My favourite movies to watch</h1>
+            <h2>My favourite movie for today is {title}</h2>
+
+            <h3>{h3}</h3>
+            <input type="text" onChange={handleChange} value={title}/>
+        </div>
+    );
 }
 
 export default App;
